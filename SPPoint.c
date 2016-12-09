ed0c3b6a27ee52b181fc0d5262432f6af971d3ef
benjamin esterlis <beni.esterlis2@gmail.com>
2016-12-09 11:10:54 +0200

    add func names

--- SPPoint.c
+++ SPPoint.c
@@ -2,5 +2,19 @@
 
 struct sp_point_t
 {
-	
+
 };
+
+SPPoint* spPointCreate(double* data, int dim, int index);
+
+SPPoint* spPointCopy(SPPoint* source);
+
+void spPointDestroy(SPPoint* point);
+
+int spPointGetDimension(SPPoint* point);
+
+int spPointGetIndex(SPPoint* point);
+
+double spPointGetAxisCoor(SPPoint* point, int axis);
+
+double spPointL2SquaredDistance(SPPoint* p, SPPoint* q);
