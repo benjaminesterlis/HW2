9855e42131b7730fffc6d1d65e64f50a05b54e73
tomersamara <xsamarax00@gmail.com>
2017-01-01 12:09:24 +0200

    tester fun added

--- unit_tests/sp_bpqueue_unit_test.c
+++ unit_tests/sp_bpqueue_unit_test.c
@@ -1,5 +1,206 @@
 #include "../SPBPriorityQueue.h"
 #include "unit_test_util.h"
 #include <stdbool.h>
+#include <stdlib.h>
+
+bool spBPQueueCreateCheck()
+{
+	int maxSize = 3;
+	SPBPQueue* q = spBPQueueCreate(maxSize);
+	ASSERT_TRUE(spBPQueueGetMaxSize(q) == maxSize);
+	ASSERT_TRUE(spBPQueueSize(q) == 0);
+	spBPQueueDestroy(q);
+	return true;
+}
+
+bool spBPQueueCopyCheck()
+{
+	int maxSize = 3;
+	SPBPQueue* q = spBPQueueCreate(maxSize);
+	SPBPQueue* q2;
+	spBPQueueEnqueue(q, 1, 3);
+	spBPQueueEnqueue(q, 5, 2);
+	spBPQueueEnqueue(q, 7, 23);
+	q2 = spBPQueueCopy(q);
+	ASSERT_TRUE(spBPQueueGetMaxSize(q) == spBPQueueGetMaxSize(q2));
+	ASSERT_TRUE(spBPQueueSize(q) == spBPQueueSize(q2));
+	ASSERT_TRUE(spBPQueueMinValue(q) == spBPQueueMinValue(q2));
+	ASSERT_TRUE(spBPQueueMaxValue(q) == spBPQueueMaxValue(q2));
+	spBPQueueDestroy(q);
+	spBPQueueDestroy(q2);
+	return true;
+}
+
+bool spBPQueueClearCheck()
+{
+	int maxSize = 3;
+	SPBPQueue* q = spBPQueueCreate(maxSize);
+	spBPQueueEnqueue(q, 1, 3);
+	spBPQueueClear(q);
+	ASSERT_TRUE(spBPQueueSize(q) == 0);
+	spBPQueueDestroy(q);
+	return true;
+}
+
+bool spBPQueueSizeCheck()
+{
+	int maxSize = 3;
+	SPBPQueue* q = spBPQueueCreate(maxSize);
+	ASSERT_TRUE(spBPQueueSize(q) == 0);
+	spBPQueueEnqueue(q, 1, 3);
+	ASSERT_TRUE(spBPQueueSize(q) == 1);
+	spBPQueueDestroy(q);
+	return true;
+}
+
+bool spBPQueueGetMaxSizeCheck()
+{
+	int maxSize = 3;
+	SPBPQueue* q = spBPQueueCreate(maxSize);
+	ASSERT_TRUE(spBPQueueGetMaxSize(q) == maxSize);
+	spBPQueueDestroy(q);
+	return true;
+}
+
+bool spBPQueueEnqueueCheck()
+{
+	int maxSize = 3;
+	SPBPQueue* q = spBPQueueCreate(maxSize);
+	ASSERT_TRUE(spBPQueueEnqueue(NULL, 2, 3) == SP_BPQUEUE_INVALID_ARGUMENT);
+	ASSERT_TRUE(spBPQueueEnqueue(q, -1, 3) == SP_BPQUEUE_INVALID_ARGUMENT);
+	ASSERT_TRUE(spBPQueueEnqueue(q, 2, -1) == SP_BPQUEUE_INVALID_ARGUMENT);
+	ASSERT_TRUE(spBPQueueEnqueue(q, 1, 3) == SP_BPQUEUE_SUCCESS);
+	ASSERT_TRUE(spBPQueueEnqueue(q, 5, 2) == SP_BPQUEUE_SUCCESS);
+	ASSERT_TRUE(spBPQueueMaxValue(q) == 3);
+	ASSERT_TRUE(spBPQueueMinValue(q) == 2);
+	ASSERT_TRUE(spBPQueueEnqueue(q, 7, 23) == SP_BPQUEUE_SUCCESS);
+	ASSERT_TRUE(spBPQueueEnqueue(q, 7, 40) == SP_BPQUEUE_FULL);
+	ASSERT_TRUE(spBPQueueEnqueue(q, 9, 23) == SP_BPQUEUE_FULL);
+	ASSERT_TRUE(spBPQueueEnqueue(q, 6, 2.5) == SP_BPQUEUE_SUCCESS);
+	ASSERT_TRUE(spBPQueueMaxValue(q) == 3);
+	spBPQueueDestroy(q);
+	return true;
+}
+
+bool spBPQueueDequeueCheck()
+{
+	int maxSize = 3;
+	SPBPQueue* q = spBPQueueCreate(maxSize);
+	ASSERT_TRUE(spBPQueueDequeue(NULL) == SP_BPQUEUE_INVALID_ARGUMENT);
+	ASSERT_TRUE(spBPQueueDequeue(q) == SP_BPQUEUE_EMPTY);
+	spBPQueueEnqueue(q, 1, 3);
+	spBPQueueEnqueue(q, 5, 2);
+	spBPQueueEnqueue(q, 7, 23);
+	ASSERT_TRUE(spBPQueueDequeue(q) == SP_BPQUEUE_SUCCESS);
+	ASSERT_TRUE(spBPQueueSize(q) == 2);
+	spBPQueueDestroy(q);
+	return true;
+}
+
+bool spBPQueuePeekCheck()
+{
+	int maxSize = 3;
+	BPQueueElement* res;
+	SPBPQueue* q = spBPQueueCreate(maxSize);
+	if ((res = (BPQueueElement*)malloc(sizeof(res))) == NULL)
+		return false;
+	ASSERT_TRUE(spBPQueuePeek(q, NULL) == SP_BPQUEUE_INVALID_ARGUMENT);
+	ASSERT_TRUE(spBPQueuePeek(NULL, res) == SP_BPQUEUE_INVALID_ARGUMENT);
+	ASSERT_TRUE(spBPQueuePeek(q, res) == SP_BPQUEUE_EMPTY);
+	spBPQueueEnqueue(q, 1, 3);
+	spBPQueueEnqueue(q, 5, 2);
+	spBPQueueEnqueue(q, 7, 23);
+	ASSERT_TRUE(spBPQueuePeek(q, res) == SP_BPQUEUE_SUCCESS);
+	ASSERT_TRUE(res->value == 2 && res->index == 5);
+	spBPQueueDestroy(q);
+	free(res);
+	return true;
+}
+
+bool spBPQueuePeekLastCheck()
+{
+	int maxSize = 3;
+	BPQueueElement* res;
+	SPBPQueue* q = spBPQueueCreate(maxSize);
+	if ((res = (BPQueueElement*)malloc(sizeof(res))) == NULL)
+		return false;
+	ASSERT_TRUE(spBPQueuePeekLast(q, NULL) == SP_BPQUEUE_INVALID_ARGUMENT);
+	ASSERT_TRUE(spBPQueuePeekLast(NULL, res) == SP_BPQUEUE_INVALID_ARGUMENT);
+	ASSERT_TRUE(spBPQueuePeekLast(q, res) == SP_BPQUEUE_EMPTY);
+	spBPQueueEnqueue(q, 1, 3);
+	spBPQueueEnqueue(q, 5, 2);
+	spBPQueueEnqueue(q, 7, 23);
+	ASSERT_TRUE(spBPQueuePeekLast(q, res) == SP_BPQUEUE_SUCCESS);
+	ASSERT_TRUE(res->value == 23 && res->index == 7);
+	spBPQueueDestroy(q);
+	free(res);
+	return true;
+}
 
 
+bool spBPQueueMinValueCheck()
+{
+	int maxSize = 3;
+	SPBPQueue* q = spBPQueueCreate(maxSize);
+	spBPQueueEnqueue(q, 1, 3);
+	spBPQueueEnqueue(q, 8, 2);
+	ASSERT_TRUE(spBPQueueMinValue(q) == 2);
+	spBPQueueEnqueue(q, 3, 1);
+	ASSERT_TRUE(spBPQueueMinValue(q) == 1);
+	spBPQueueDestroy(q);
+	return true;
+}
+
+bool spBPQueueMaxValueCheck()
+{
+	int maxSize = 3;
+	SPBPQueue* q = spBPQueueCreate(maxSize);
+	spBPQueueEnqueue(q, 1, 3);
+	spBPQueueEnqueue(q, 6, 2);
+	ASSERT_TRUE(spBPQueueMaxValue(q) == 3);
+	spBPQueueEnqueue(q, 8, 23);
+	ASSERT_TRUE(spBPQueueMaxValue(q) == 23);
+	spBPQueueDestroy(q);
+	return true;
+}
+
+bool spBPQueueIsEmptyCheck()
+{
+	int maxSize = 3;
+	SPBPQueue* q = spBPQueueCreate(maxSize);
+	ASSERT_TRUE(spBPQueueIsEmpty(q) == true);
+	spBPQueueEnqueue(q, 1, 3);
+	ASSERT_TRUE(spBPQueueIsEmpty(q) == false);
+	spBPQueueDestroy(q);
+	return true;
+}
+
+bool spBPQueueIsFullCheck()
+{
+	int maxSize = 1;
+	SPBPQueue* q = spBPQueueCreate(maxSize);
+	ASSERT_TRUE(spBPQueueIsFull(q) == false);
+	spBPQueueEnqueue(q, 1, 3);
+	ASSERT_TRUE(spBPQueueIsFull(q) == true);
+	spBPQueueDestroy(q);
+	return true;
+}
+
+int main()
+{
+	RUN_TEST(spBPQueueCreateCheck);
+	RUN_TEST(spBPQueueCopyCheck);
+	RUN_TEST(spBPQueueClearCheck);
+	RUN_TEST(spBPQueueSizeCheck);
+	RUN_TEST(spBPQueueGetMaxSizeCheck);
+	RUN_TEST(spBPQueueEnqueueCheck);
+	RUN_TEST(spBPQueueDequeueCheck);
+	RUN_TEST(spBPQueuePeekCheck);
+	RUN_TEST(spBPQueuePeekLastCheck);
+	RUN_TEST(spBPQueueMinValueCheck);
+	RUN_TEST(spBPQueueMaxValueCheck);
+	RUN_TEST(spBPQueueIsEmptyCheck);
+	RUN_TEST(spBPQueueIsFullCheck);
+
+	return 0;
+}
--- unit_tests/sp_point_unit_test.c
+++ unit_tests/sp_point_unit_test.c
@@ -2,23 +2,84 @@
 #include "unit_test_util.h"
 #include <stdbool.h>
 
-//Checks if copy Works
-bool pointBasicCopyTest() {
+bool spPointCreateCheck()
+{
 	double data[2] = { 1.0, 1.0 };
 	int dim = 2;
 	int index = 1;
+	int i;
+	SPPoint* p = spPointCreate(data, dim, index);
+	ASSERT_TRUE(spPointCreate(NULL, dim, index) == NULL);
+	ASSERT_TRUE(spPointCreate(data , 0, index) == NULL);
+	ASSERT_TRUE(spPointCreate(NULL, dim, -1) == NULL);
+	ASSERT_TRUE(spPointGetIndex(p) == index);
+	ASSERT_TRUE(spPointGetDimension(p) == dim);
+	for (i = 0; i < spPointGetDimension(p); ++i)
+	{
+		ASSERT_TRUE(spPointGetAxisCoor(p, i) == data[i]);
+	}
+	spPointDestroy(p);
+	return true;
+}
+
+bool spPointCopyCheck()
+{
+	double data[2] = { 1.0, 1.0 };
+	int dim = 2;
+	int index = 1;
+	int i;
 	SPPoint* p = spPointCreate(data, dim, index);
 	SPPoint* q = spPointCopy(p);
 	ASSERT_TRUE(spPointGetIndex(p) == spPointGetIndex(q));
 	ASSERT_TRUE(spPointGetDimension(p) == spPointGetDimension(q));
-	for (int i = 0; i < spPointGetDimension(p); i++) {
+	for (i = 0; i < spPointGetDimension(p); ++i)
+	{
 		ASSERT_TRUE(spPointGetAxisCoor(p, i) == spPointGetAxisCoor(q, i));
 	}
 	spPointDestroy(p);
 	spPointDestroy(q);
 	return true;
 }
-bool pointBasicL2Distance() {
+
+bool spPointGetDimensionCheck()
+{
+	double data[2] = { 1.0, 1.0 };
+	int dim = 2;
+	int index = 1;
+	SPPoint* p = spPointCreate(data, dim, index);
+	ASSERT_TRUE(spPointGetDimension(p) == dim);
+	spPointDestroy(p);
+	return true;
+}
+
+bool spPointGetIndexCheck()
+{
+	double data[2] = { 1.0, 1.0 };
+	int dim = 2;
+	int index = 1;
+	SPPoint* p = spPointCreate(data, dim, index);
+	ASSERT_TRUE(spPointGetIndex(p) == index);
+	spPointDestroy(p);
+	return true;
+}
+
+bool spPointGetAxisCoorCheck()
+{
+	double data[2] = { 1.0, 1.0 };
+	int dim = 2;
+	int index = 1;
+	int i;
+	SPPoint* p = spPointCreate(data, dim, index);
+	for (i = 0; i < dim; ++i)
+	{
+		ASSERT_TRUE(spPointGetAxisCoor(p, i) == data[i]);
+	}
+	spPointDestroy(p);
+	return true;
+}
+
+bool spPointL2SquaredDistanceCheck()
+{
 	double data1[2] = { 1.0, 1.0 };
 	double data2[2] = { 1.0, 0.0 };
 	int dim1 = 2;
@@ -27,15 +88,20 @@ bool pointBasicL2Distance() {
 	int index2 = 1;
 	SPPoint* p = spPointCreate(data1, dim1, index1);
 	SPPoint* q = spPointCreate(data2, dim2, index2);
-	ASSERT_TRUE(spPointL2SquaredDistance(p,p) == 0.0);
-	ASSERT_TRUE(spPointL2SquaredDistance(q,q) == 0.0);
-	ASSERT_FALSE(spPointL2SquaredDistance(p,q) == 0.0);
+	ASSERT_TRUE(spPointL2SquaredDistance(p, p) == 0.0);
+	ASSERT_TRUE(spPointL2SquaredDistance(q, q) == 0.0);
+	ASSERT_FALSE(spPointL2SquaredDistance(p, q) == 0.0);
 	spPointDestroy(p);
 	spPointDestroy(q);
 	return true;
 }
-int main() {
-	RUN_TEST(pointBasicCopyTest);
-	RUN_TEST(pointBasicL2Distance);
+
+int main()
+{
+	RUN_TEST(spPointCreateCheck);
+	RUN_TEST(spPointCopyCheck);
+	RUN_TEST(spPointGetDimensionCheck);
+	RUN_TEST(spPointGetIndexCheck);
+	RUN_TEST(spPointGetAxisCoorCheck);
+	RUN_TEST(spPointL2SquaredDistanceCheck);
 	return 0;
-}
